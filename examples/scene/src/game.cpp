ab0f2e779dddd0b058147cb81b5b5b1ce2e34324
denghe <denghe@hotmail.com>
2025-08-18 17:46:06 +0800

    add new example: scene

---
+++ examples/scene/src/game.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "game.h"
+Game gg;
+
+int32_t main() {
+	return gg.Run();
+}
+
+void Game::Init() {
+	title = U"examples_scene";
+	showStat = true;
+}
+
+xx::Task<> Game::Task() {
+	auto tex = LoadTexture("res/1.png");
+	auto rect = tex->Rect();
+	while (true) {
+		auto& q = Quad();
+		auto basePos = -windowSize / 2.f;
+		for (int i = 0; i < 100; ++i) {
+			for (int j = 0; j < 100; ++j) {
+				q.Draw(tex, rect, basePos + XY{ i * rect.h, j * rect.w }, 0);
+			}
+		}
+		co_yield 0;
+	}
+}
--- examples/scene/src/game.h
+++ examples/scene/src/game.h
@@ -2,6 +2,7 @@
 #include "pch.h"
 using XY = xx::XY;
 struct Game : xx::Game<Game> {
+	static constexpr float fps{ 120 }, frameDelay{ 1.f / fps }, maxFrameDelay{ 0.1f };
 	void Init();
 	xx::Task<> Task();
 };
