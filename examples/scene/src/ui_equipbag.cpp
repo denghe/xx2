13880b3171359fc082c04ce631de24a98b3e701c
denghe <denghe@hotmail.com>
2025-09-17 03:19:20 +0800

    ...

--- examples/scene/src/ui_equipbag.cpp
+++ examples/scene/src/ui_equipbag.cpp
@@ -27,9 +27,13 @@ size: 480, 1080
 
 	void EquipBag::Init(int32_t z_) {
 		children.Clear();	// unsafe
-        InitDerived<EquipBag>(z_, gg.p1, gg.a1, 1, { gg.designSize.x / 4, gg.designSize.y });
 
-        // border
+        static constexpr float cMargin{ 15 };
+        static constexpr XY cCellSize{ 64, 64 };
+        static constexpr float cCellMargin{ 5 };
+        XY totalSize{ gg.designSize.x / 4, gg.designSize.y };
+
+        InitDerived<EquipBag>(z_, gg.p1, gg.a1, 1, totalSize);
         Make<xx::Scale9>()->Init(z + 1, 0, 0, size);
 
         //// swallow
@@ -37,6 +41,49 @@ size: 480, 1080
         //    xx::CoutN("EquipBag clicked");
         //};
 
-        // todo
+        // helm
+        XY offset{ totalSize.x / 2, totalSize.y - cMargin - cCellSize.y / 2 };
+        Make<EquipBagCell>()->Init(z + 2, offset, 0.5f, cCellSize);
+
+        // ring
+        offset -= cCellSize;
+        Make<EquipBagCell>()->Init(z + 2, offset, 0.5f, cCellSize);
+
+        // amulet
+        offset.x += cCellSize.x * 2;
+        Make<EquipBagCell>()->Init(z + 2, offset, 0.5f, cCellSize);
+
+        // armor
+        offset -= cCellSize;
+        Make<EquipBagCell>()->Init(z + 2, offset, 0.5f, cCellSize);
+
+        // weapon1
+        offset -= cCellSize;
+        Make<EquipBagCell>()->Init(z + 2, offset, 0.5f, cCellSize);
+
+        // weapon2
+        offset.x += cCellSize.x * 2;
+        Make<EquipBagCell>()->Init(z + 2, offset, 0.5f, cCellSize);
+
+        // boots
+        offset -= cCellSize;
+        Make<EquipBagCell>()->Init(z + 2, offset, 0.5f, cCellSize);
+
+        offset.y -= cCellSize.y / 2 + cMargin;
+        XY cellsSize{ totalSize.x - cMargin * 2, offset.y - cMargin };
+        //Make<EquipBagCell>()->Init(z + 2, { cMargin, offset.y }, { 0, 1 }, cellsSize);
+
+        auto w = int32_t(cellsSize.x + cCellMargin) / int32_t(cCellSize.x + cCellMargin);
+        auto h = int32_t(cellsSize.y + cCellMargin) / int32_t(cCellSize.y + cCellMargin);
+        auto cCellMarginH = (cellsSize.x - cCellSize.x * w) / (w - 1);
+        auto cCellMarginV = (cellsSize.y - cCellSize.y * h) / (h - 1);
+        XY basePos{ cMargin }, pos{};
+        for (int32_t y = 0; y < h; ++y) {
+            for (int32_t x = 0; x < w; ++x) {
+                pos.x = basePos.x + x * (cCellSize.x + cCellMarginH);
+                pos.y = basePos.y + cellsSize.y - y * (cCellSize.y + cCellMarginV);
+                Make<EquipBagCell>()->Init(z + 2, pos, {0,1}, cCellSize);
+            }
+        }
 	}
 }
--- examples/scene/src/ui_equipbag.h
+++ examples/scene/src/ui_equipbag.h
@@ -3,8 +3,22 @@
 
 namespace UI {
 
-	struct EquipBagCell : xx::Node {
+	struct EquipBagCell : xx::Button {
 		static constexpr int32_t cTypeId{ 104 };
+		EquipBagCell& Init(int32_t z_, XY position_, XY anchor_, XY size_) {
+			assert(typeId == cTypeId);
+			z = z_;
+			position = position_;
+			anchor = anchor_;
+			size = size_;
+			cfgNormal = gg.embed.cfg_s9bN;
+			cfgHighlight = gg.embed.cfg_s9bH;
+			FillTrans();
+			auto& cfg = GetCfg();
+			Make<xx::Scale9>()->Init(z + 1, 0, 0, size, cfg);
+			return *this;
+		}
+
 	};
 
 	struct EquipBag : xx::Node {
